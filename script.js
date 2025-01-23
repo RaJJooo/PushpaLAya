1f1382afb42e2445940cd2bdc203eeb2d0f12620
Rajashree Sardar <rajashreesardar257@gmail.com>
2025-01-24 02:29:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+new Swiper('.card-wrapper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  spaceBetween:30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+    0:{
+      slidesPerView:1
+    },
+    // 768:{
+    //   slidesPerView:2
+    // },
+    // 1024:{
+    //   slidesPerView:3
+    // },
+  }
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
